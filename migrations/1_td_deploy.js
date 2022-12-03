b6042a8fdb4f27c1e8f4a331ca99b959335b7b5c
Antoine Sirot <101109062+AntoineSirot@users.noreply.github.com>
2022-12-03 13:42:18 +0100

    Update 1_td_deploy.js

--- migrations/1_td_deploy.js
+++ migrations/1_td_deploy.js
@@ -1,52 +1,106 @@
-const Str = require('@supercharge/strings')
-// const BigNumber = require('bignumber.js');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+var privatekey = "YOUR_PRIVATE_KEY"
+var infuraApiKey = "YOUR_INFURA_KEY"
+/**
+ * Use this file to configure your truffle project. It's seeded with some
+ * common settings for different networks and features like migrations,
+ * compilation and testing. Uncomment the ones you need or modify
+ * them to suit your project as necessary.
+ *
+ * More information about configuration can be found at:
+ *
+ * trufflesuite.com/docs/advanced/configuration
+ *
+ * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
+ * to sign your transactions before they're sent to a remote public node. Infura accounts
+ * are available for free at: infura.io/register.
+ *
+ * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
+ * public/private key pairs. If you're publishing your code to GitHub make sure you load this
+ * phrase from a file you've .gitignored so it doesn't accidentally become public.
+ *
+ */
 
-var TDErc20 = artifacts.require("ERC20TD.sol");
-var evaluator = artifacts.require("Evaluator.sol");
+// const HDWalletProvider = require('@truffle/hdwallet-provider');
+// const infuraKey = "fj4jll3k.....";
+//
+// const fs = require('fs');
+// const mnemonic = fs.readFileSync(".secret").toString().trim();
 
+module.exports = {
+  /**
+   * Networks define how you connect to your ethereum client and let you set the
+   * defaults web3 uses to send transactions. If you don't specify one truffle
+   * will spin up a development blockchain for you on port 9545 when you
+   * run `develop` or `test`. You can ask a truffle command to use a specific
+   * network from the command line, e.g
+   *
+   * $ truffle test --network <network-name>
+   */
 
-module.exports = (deployer, network, accounts) => {
-    deployer.then(async () => {
-        await deployTDToken(deployer, network, accounts); 
-        await deployEvaluator(deployer, network, accounts); 
-        await setPermissionsAndRandomValues(deployer, network, accounts); 
-        await deployRecap(deployer, network, accounts); 
-    });
+  networks: {
+    goerli: {
+      provider: function(){
+        return new HDWalletProvider(privatekey, "https://goerli.infura.io/v3/"  + infuraApiKey)
+      },
+      network_id: 5,
+      networkCheckTimeout: 30000
+    }
+    // Useful for testing. The `development` name is special - truffle uses it by default
+    // if it's defined here and no other network is specified at the command line.
+    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
+    // tab if you use this network and you must also set the `host`, `port` and `network_id`
+    // options below to some value.
+    //
+    // development: {
+    //  host: "127.0.0.1",     // Localhost (default: none)
+    //  port: 8545,            // Standard Ethereum port (default: none)
+    //  network_id: "*",       // Any network (default: none)
+    // },
+    // Another network with more advanced options...
+    // advanced: {
+    // port: 8777,             // Custom port
+    // network_id: 1342,       // Custom network
+    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+    // from: <address>,        // Account to send txs from (default: accounts[0])
+    // websockets: true        // Enable EventEmitter interface for web3 (default: false)
+    // },
+    // Useful for deploying to a public network.
+    // NB: It's important to wrap the provider as a function.
+    // ropsten: {
+    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
+    // network_id: 3,       // Ropsten's id
+    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    // },
+    // Useful for private networks
+    // private: {
+    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
+    // network_id: 2111,   // This network is yours, in the cloud.
+    // production: true    // Treats this network as if it was a public net. (default: false)
+    // }
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.6.2",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  }
 };
-
-async function deployTDToken(deployer, network, accounts) {
-	TDToken = await TDErc20.new("TD-ERC20-101","TD-ERC20-101",web3.utils.toBN("20000000000000000000000000000"))
-}
-
-async function deployEvaluator(deployer, network, accounts) {
-	Evaluator = await evaluator.new(TDToken.address)
-}
-
-async function setPermissionsAndRandomValues(deployer, network, accounts) {
-	await TDToken.setTeacher(Evaluator.address, true)
-	randomSupplies = []
-	randomTickers = []
-	for (i = 0; i < 20; i++)
-		{
-		randomSupplies.push(Math.floor(Math.random()*1000000000))
-		randomTickers.push(Str.random(5))
-		// randomTickers.push(web3.utils.utf8ToBytes(Str.random(5)))
-		// randomTickers.push(Str.random(5))
-		}
-
-	console.log(randomTickers)
-	console.log(randomSupplies)
-	// console.log(web3.utils)
-	// console.log(type(Str.random(5)0)
-	await Evaluator.setRandomTickersAndSupply(randomSupplies, randomTickers);
-}
-
-async function deployRecap(deployer, network, accounts) {
-	console.log("TDToken " + TDToken.address)
-	console.log("Evaluator " + Evaluator.address)
-}
-
-
-// truffle run verify ERC20TD@0x09f14a40Fd672B5B056FF8b5c343498452CAC4b2 --network goerli
-// truffle run verify evaluator@0x7C5629d850eCD1E640b1572bC0d4ac5210b38FA5 --network goerli
-
